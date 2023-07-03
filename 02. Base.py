6222211b32b27b9d313f51be5d061d0608ab89fb
{alacid95} <{alejandroalacid95@gmail.com}>
2023-07-03 18:06:28 +0200

    FizzBuzz

---
+++ 02. Base.py
@@ -0,0 +1,32 @@
+"""
+print('\nEjercicio 2\n')
+var = "Hola Mundo!"
+print(var)
+
+
+print('\nEjercicio 3\n')
+nombre = input('Introduce tu nombre: ')
+print ("Hola: ", nombre)
+
+
+print('\nEjercicio 4\n')
+var = pow((3+2)/(2*5),2)
+print(var)
+
+print('\nEjercicio 5\n')
+horas = input('Nº horas trabajadas: ')
+coste = input('Coste por hora: ')
+print (horas*coste)
+"""
+
+#fizzbuzz
+for i in range(100):
+    var = i
+
+    if i%3 == 0:
+        var = 'Fizz'
+        if i%5 == 0:
+            var = 'FizzBuzz'
+    elif i % 5 == 0:
+        var = 'Buzz'
+    print (var)
